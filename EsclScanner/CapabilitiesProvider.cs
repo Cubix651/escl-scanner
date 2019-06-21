f17c50c8e2dc2958efdb86acea544814c6872ef6
Jakub Cisło <cubix651@gmail.com>
2019-06-21 17:15:31 +0200

    Create CapabilitiesProvider

---
+++ EsclScanner/CapabilitiesProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EsclScanner
+{
+    public class CapabilitiesProvider
+    {
+        public static readonly string CAPABILITIES_URI_PATTERN = "http://{0}/eSCL/ScannerStatus";
+
+        private IEsclClient esclClient;
+        private string endpoint;
+
+        public CapabilitiesProvider(IEsclClient esclClient, string host)
+        {
+            this.esclClient = esclClient;
+            this.endpoint = String.Format(CAPABILITIES_URI_PATTERN, host);
+        }
+
+        public async Task<CapabilitiesInfo?> GetCapabilities()
+        {
+            var response = await esclClient.GetAsync(endpoint);
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var content = response.Content;
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(content);
+            return null;
+        }
+    }
+}
