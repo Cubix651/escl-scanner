9781221b6a34c09130c1d55ec06fdd53ef675a2a
Jakub Cisło <cubix651@gmail.com>
2019-06-21 18:43:19 +0200

    Create job creator

---
+++ EsclScanner/EsclJobCreator.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+
+namespace EsclScanner
+{
+    public class EsclJobCreator
+    {
+        public static readonly string NEW_JOB_URI_PATTERN = "http://{0}/eSCL/ScanJobs";
+
+        IEsclClient esclClient;
+        string endpoint;
+        EsclScanRequestGenerator scanRequestGenerator;   
+
+        public EsclJobCreator(IEsclClient esclClient,
+                              string host,
+                              EsclScanRequestGenerator scanRequestGenerator)
+        {
+            this.esclClient = esclClient;
+            this.endpoint = string.Format(NEW_JOB_URI_PATTERN, host);
+        }
+
+        public async Task<string> CreateJob()
+        {
+            string request = scanRequestGenerator.Generate();
+            var response = await esclClient.PostAsync(endpoint, request);
+            return response.LocationHeader.PathAndQuery;
+        }
+        
+    }
+}
