c4ce7854d9d60b9b1222c2d8f1e2846eb424310a
Jakub Cisło <cubix651@gmail.com>
2019-06-21 15:24:53 +0200

    Create EsclResponse

---
+++ EsclScanner/EsclResponse.cs
@@ -0,0 +1,26 @@
+using System.Net.Http;
+
+namespace EsclScanner
+{
+    public class EsclResponse : IEsclResponse
+    {
+        HttpResponseMessage response;
+        string content = null;
+
+        public EsclResponse(HttpResponseMessage response)
+        {
+            this.response = response;
+        }
+        public bool IsSuccessStatusCode => response.IsSuccessStatusCode;
+
+        public string Content
+        {
+            get
+            {
+                if (content == null)
+                    content = response.Content.ReadAsStringAsync().Result;
+                return content;
+            }
+        }
+    }
+}
