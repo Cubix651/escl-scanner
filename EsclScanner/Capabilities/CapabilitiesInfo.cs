3954ab3e1f4d2a6cc538f12904cde94051319081
Jakub Cisło <cubix651@gmail.com>
2019-06-24 19:10:05 +0200

    Add new capabilities to info struct

--- EsclScanner/Capabilities/CapabilitiesInfo.cs
+++ EsclScanner/Capabilities/CapabilitiesInfo.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using Escl.Utils;
+
 namespace Escl.Capabilities
 {
     public struct CapabilitiesInfo
     {
-        public string Model {get;set;}
+        public string Model {get; set;}
+        public int MinWidth {get; set;}
+        public int MaxWidth {get; set;}
+        public int MinHeight {get; set;}
+        public int MaxHeight {get; set;}
+        public List<string> DocumentFormatExtensions {get; set;}
+        public List<Resolution> Resolutions {get; set;}
     }
 }
