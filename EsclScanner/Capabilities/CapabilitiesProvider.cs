5d0d05a49a6ad9ab0e426bfd12df3f7f53d3f45b
Jakub Cisło <cubix651@gmail.com>
2019-06-24 19:11:13 +0200

    Add extractor inside CapabilitiesProvider

--- EsclScanner/Capabilities/CapabilitiesProvider.cs
+++ EsclScanner/Capabilities/CapabilitiesProvider.cs
@@ -2,6 +2,7 @@
 using Escl.Connection;
 using System;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Escl.Capabilities
 {
@@ -18,15 +19,36 @@ namespace Escl.Capabilities
             this.endpoint = String.Format(CAPABILITIES_URI_PATTERN, host);
         }
 
+        private class Extractor
+        {
+            XmlDocument xml;
+            XmlNamespaceManager namespaceManager;
+
+            public Extractor(XmlDocument xml, XmlNamespaceManager namespaceManager)
+            {
+                this.xml = xml;
+                this.namespaceManager = namespaceManager;
+            }
+
+            public string Extract(string xpath)
+            {
+                var node = xml.SelectSingleNode(xpath, namespaceManager);
+                return node?.InnerText;
+            }
+
+        }
+
         public async Task<CapabilitiesInfo> GetCapabilities()
         {
             var response = await esclClient.GetAsync(endpoint);
             var xml = response.Content;
             var namespaceManager = NamespaceUtils.CreateNamespaceManager(xml);
-            var modelNode = xml.SelectSingleNode("/scan:ScannerCapabilities/pwg:MakeAndModel",
-                                                 namespaceManager);
-            string model = modelNode?.InnerText;
-            return new CapabilitiesInfo { Model = model };
+            var extractor = new Extractor(xml, namespaceManager);
+            
+            return new CapabilitiesInfo
+            {
+                Model = extractor.Extract("/scan:ScannerCapabilities/pwg:MakeAndModel")
+            };
         }
     }
 }
