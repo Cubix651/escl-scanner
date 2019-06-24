f869b938a26248b09236951033b431ec4a0d04da
Jakub Cisło <cubix651@gmail.com>
2019-06-24 20:28:05 +0200

    Add unit tests for capabilities

--- EsclScannerTest/CapabilitiesProviderTest.cs
+++ EsclScannerTest/CapabilitiesProviderTest.cs
@@ -1,9 +1,11 @@
 using Xunit;
 using FakeItEasy;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Xml;
 using Escl.Connection;
 using Escl.Capabilities;
+using Escl.Utils;
 
 namespace EsclScannerTest
 {
@@ -90,34 +92,127 @@ namespace EsclScannerTest
 <scan:ScannerCapabilities xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:scan=""http://schemas.hp.com/imaging/escl/2011/05/03"" xmlns:pwg=""http://www.pwg.org/schemas/2010/12/sm"" xsi:schemaLocation=""http://schemas.hp.com/imaging/escl/2011/05/03 eSCL.xsd"">
 </scan:ScannerCapabilities>";
 
-        [Fact]
-        public async Task successful_response_with_model_name()
+        private async Task<CapabilitiesInfo> GetCapabilities(string xmlResponse)
         {
             var responseXml = new XmlDocument();
-            responseXml.LoadXml(FULL_RESPONSE_BODY);
+            responseXml.LoadXml(xmlResponse);
             var client = A.Fake<IEsclClient>();
             A.CallTo(() => client.GetAsync("http://192.168.0.151/eSCL/ScannerCapabilities"))
                 .Returns(Task.FromResult<IEsclResponse>(new EsclResponse(content: responseXml)));
 
             var capabilitiesProvider = new CapabilitiesProvider(client, "192.168.0.151");
             var capabilities = await capabilitiesProvider.GetCapabilities();
+            return capabilities;
+        }
 
+        [Fact]
+        public async Task response_with_model_name()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
             Assert.Equal("HP LaserJet MFP M28w", capabilities.Model);
         }
         
         [Fact]
-        public async Task successful_response_without_model_name()
+        public async Task response_without_model_name()
         {
-            var responseXml = new XmlDocument();
-            responseXml.LoadXml(EMPTY_RESPONSE_BODY);
-            var client = A.Fake<IEsclClient>();
-            A.CallTo(() => client.GetAsync("http://192.168.0.151/eSCL/ScannerCapabilities"))
-                .Returns(Task.FromResult<IEsclResponse>(new EsclResponse(content: responseXml)));
-
-            var capabilitiesProvider = new CapabilitiesProvider(client, "192.168.0.151");
-            var capabilities = await capabilitiesProvider.GetCapabilities();
-
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
             Assert.Null(capabilities.Model);
+        }
+
+        [Fact]
+        public async Task response_with_min_width()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+            Assert.Equal(300, capabilities.MinWidth);
+        }
+        
+        [Fact]
+        public async Task response_without_min_width()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Equal(0, capabilities.MinWidth);
+        }
+
+        [Fact]
+        public async Task response_with_min_height()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+            Assert.Equal(300, capabilities.MinHeight);
+        }
+        
+        [Fact]
+        public async Task response_without_min_height()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Equal(0, capabilities.MinHeight);
+        }
+
+        [Fact]
+        public async Task response_with_max_width()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+            Assert.Equal(2550, capabilities.MaxWidth);
+        }
+        
+        [Fact]
+        public async Task response_without_max_width()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Equal(0, capabilities.MaxWidth);
+        }
+
+        [Fact]
+        public async Task response_with_max_height()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+            Assert.Equal(3508, capabilities.MaxHeight);
+        }
+        
+        [Fact]
+        public async Task response_without_max_height()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Equal(0, capabilities.MaxHeight);
+        }
+
+        [Fact]
+        public async Task response_with_document_format_ext()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+            var formats = capabilities.DocumentFormatExtensions;
+            formats.Sort();
+
+            Assert.Equal(
+                new List<string> {"application/octet-stream","application/pdf","image/jpeg"},
+                formats);
+        }
+        
+        [Fact]
+        public async Task response_without_document_format_ext()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Empty(capabilities.DocumentFormatExtensions);
+        }
+
+        [Fact]
+        public async Task response_with_resolutions()
+        {
+            var capabilities = await GetCapabilities(FULL_RESPONSE_BODY);
+
+            Assert.Equal(
+                new List<Resolution> {
+                    new Resolution {X = 200, Y = 200},
+                    new Resolution {X = 300, Y = 300},
+                    new Resolution {X = 600, Y = 600},
+                },
+                capabilities.Resolutions);
+        }
+        
+        [Fact]
+        public async Task response_without_resolutions()
+        {
+            var capabilities = await GetCapabilities(EMPTY_RESPONSE_BODY);
+            Assert.Empty(capabilities.Resolutions);
         }
     }
 }
