96eda0dead12bf2e189aae33af715f95b1a38f2d
Jakub Cisło <cubix651@gmail.com>
2019-06-25 22:27:20 +0200

    Modify welcome View and ViewModel

--- EsclScannerGui/ViewModels/WelcomeViewModel.cs
+++ EsclScannerGui/ViewModels/WelcomeViewModel.cs
@@ -1,6 +1,8 @@
 namespace EsclScannerGui.ViewModels
 {
     public class WelcomeViewModel : ViewModelBase
-    {        
+    {
+        public string WelcomeMessage => "Welcome to eSCL Scan Tool!";
+        public string Description => "Fetching scanner info...";
     }
 }
